61c18e4cbbba77661ffdc86def98d415ee064c37
Erdozain Arlegui <D692832@admon-cfnavarra.es>
2016-01-14 15:11:59 +0100

    reestructuracion

--- start.js
+++ start.js
@@ -1,113 +1,14 @@
-//var connect = require('connect');
-var fs = require('fs');
-var httpServer = require('http');
-var path = require('path');
-var Url = require('url');
+var express = require('express');
+var path    = require("path");
+var app     = express();
 
+app.use(express.static('Cliente'));
+app.get('/',function (req, res) {  
+  res.sendFile(path.join(__dirname+'/Cliente/index.html'));    
+});
 
-// localhost
+//require('./app/controllers/leds')(app);
 
-var httpPort = process.env.PORT || 8000;
-
-/* 
- 
- see README.md for a more detailed write up 
-
-*/
-
-//////////////////////////////////////////////////////// HTTP - sends html/js/css to the browswer 
-
-var sendHTML = function( filePath, contentType, response ){
-
-  //console.log('sendHTML: ' + filePath) ;
-
-  var pathSinParams = filePath.split('?')[0];
-  if (pathSinParams.substring(pathSinParams.length - 1)=='?') 
-    pathSinParams = pathSinParams.substring(0,str.length - 1);
-
-  fs.exists(pathSinParams, function( exists ) {
-
-        console.log(pathSinParams);
-        if (exists) {
-            fs.readFile(pathSinParams, function(error, content) {
-                if (error) {
-                    response.writeHead(500);
-                    response.end();
-                }
-                else {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                }
-            });
-        }
-        else {
-            response.writeHead(404);
-            response.end();
-        }
-    });
-}
-
-var getFilePath = function(url) {
-  var filePath = './' + url;
-  if (url == '/' ) filePath = './index.html';
-  return filePath;
-}
-
-var getContentType = function(filePath) {
-   
-   //var extname = path.extname(filePath);
-   var extname = path.extname(Url.parse(filePath).pathname)
-   var contentType = 'text/html';    
-    console.log(filePath);
-    console.log(extname);
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.ttf':
-        	contentType = 'font/ttf';
-          break;
-        case '.otf':        
-          contentType = 'font/opentype';
-          break;
-        case '.woff':
-       
-          contentType = 'application/binary';
-          break;
-        case '.eot':
-       
-          contentType = 'application/binary';
-          break;
-    }
-    console.log(contentType);
-    return contentType;
-}
-
-var onHtmlRequestHandler = function(request, response) {
-
-  //console.log('onHtmlRequestHandler... request.url: ' + request.url) ;
-
-  /*
-   when this is live, nodjitsu only listens on 1 port(80) so the httpServer will hear it first but
-   we need to direct the request to the mongodbServer
-   */
-  if ( process.env.PORT && url === '/messages') {
-    
-    // pass the request to mongodbServer
-
-    return; 
-  } 
-
-  var filePath = getFilePath(request.url);
-  var contentType = getContentType(filePath);
-
-  //console.log('onHtmlRequestHandler... getting: ' + filePath) ;
-
-  sendHTML(filePath, contentType, response); 
-
-}
-
-httpServer.createServer(onHtmlRequestHandler).listen(httpPort); 
+app.listen(3000, function() {
+  console.log('Raspi Express server listening...');
+});
